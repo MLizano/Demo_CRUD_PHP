e28e633063f67d8bee53d11a5e5b279a7e8c9ec5
MLizano <lizanocr@hotmail.com>
2021-11-10 08:50:34 -0600

    creacion de carpetas
    creacion de carpetas y subcarpetas, con sus archivos y plugins

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+   <title>Login</title>
+
+   <link rel="stylesheet" href="bootstrap/css/bootstrap" >
+
+</head>
+
+<body>
+   
+
+
+
+
+</body>
+</html>
