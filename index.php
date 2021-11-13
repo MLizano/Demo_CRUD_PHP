4531e505c0430d5f35d7c8b9ae22c0d427461932
MLizano <lizanocr@hotmail.com>
2021-11-13 00:40:34 -0600

    Login terminado

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 
    <title>Login</title>
 
+   <!-- Bootstrap CSS -->
    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" >
    <link rel="stylesheet" href="estilos.css" >
 
@@ -17,18 +18,44 @@
 
 <body>
    
+   <div id="login">
+      <h3 class="text-center text-white display-4">Login con PHP</h3>
 
-
-
-
+      <div class="container">                        
+         <div id="login-row" class="row justify-content-center align-items-center">
+               <div id="login-column" class="col-md-6">
+                  <div id="login-box" class="col-md-12  bg-light text-dark">
+                     <form id="formLogin" class="form" action="" method="post">
+                           <h3 class="text-center text-dark">Iniciar Sesión</h3>
+                           <div class="form-group">
+                              <label for="usuario" class="text-dark">Usuario</label><br>
+                              <input type="text" name="usuario" id="usuario" class="form-control">
+                           </div>
+                           <div class="form-group">
+                              <label for="password" class="text-dark">Password</label><br>
+                              <input type="password" name="password" id="password" class="form-control">
+                           </div>
+                           <div class="form-group text-center">                                
+                              <input type="submit" name="submit" class="btn btn-dark btn-lg btn-block" value="Conectar">
+                           </div>                            
+                     </form>
+                  </div>
+               </div>
+         </div>
+      </div>
+   </div>
 
 
 <!-- /////////////////////////////////////////////////////////// -->
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
 <script src="jquery/jquery-3.6.0.min.js"></script>
-<script src="bootstrap/js/bootstrap.min.js"></script>
 <script src="popper/popper.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
 
 <script src="plugins/sweetalert2.all.min.js"></script>
+<script src="codigo.js"></script>
 
 </body>
 </html>
