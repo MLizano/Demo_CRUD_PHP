8a9bf8e8f3f68189b26993d8fa6ef05cd1193f5f
MLizano <lizanocr@hotmail.com>
2021-11-12 23:22:27 -0600

    estructura de index

--- index.php
+++ index.php
@@ -8,8 +8,11 @@
 
    <title>Login</title>
 
-   <link rel="stylesheet" href="bootstrap/css/bootstrap" >
+   <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" >
+   <link rel="stylesheet" href="estilos.css" >
 
+   <link rel="stylesheet" href="plugins/sweetalert2.min.css" >
+ 
 </head>
 
 <body>
@@ -17,6 +20,15 @@
 
 
 
+
+
+
+<!-- /////////////////////////////////////////////////////////// -->
+<script src="jquery/jquery-3.6.0.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<script src="popper/popper.min.js"></script>
+
+<script src="plugins/sweetalert2.all.min.js"></script>
 
 </body>
 </html>
